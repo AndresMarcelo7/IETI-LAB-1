c00987ae997a8ddba210947925ae4c76ccf25dd2
Andres Marcelo <felipemarcelo156@gmail.com>
2020-08-15 13:27:46 -0500

    First part

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+export class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <div>
+          <h2>{this.props.text}</h2>
+          <p>{this.props.priority}</p>
+          <p>{this.props.dueDate.toString()}</p>
+        </div>
+    );
+  }
+}
---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Todo } from "./Todo";
+export class TodoList extends React.Component {
+  render() {
+    const toDos = this.props.todos;
+    const listItems = toDos.map((todo,i) =>
+        <li key={i}>
+            <Todo text={todo.text}
+             priority={todo.priority}
+             dueDate={todo.dueDate}/>
+        </li>);
+    return (<ul>{listItems}</ul>);
+  }
+}
